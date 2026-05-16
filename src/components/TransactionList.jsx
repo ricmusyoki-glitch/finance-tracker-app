61040cb0a3551ff829039fb4483622ba34716040
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-16 11:34:56 +0300

    Modified TransactionList.jsx

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,25 +1,18 @@
 import TransactionItem from './TransactionItem'
 
-const TransactionList = ({
-  transactions, deleteTransaction
-}) => {
-
-     
+const TransactionList = ({ transactions, deleteTransaction }) => {
   return (
-
-    <div className='space-y-4'>
-
-        {transactions.map((transaction) => 
-        
-        <TransactionItem 
-        key={transaction.id}
-        title={transaction.title}
-        amount={transaction.amount}
-        onDelete = {() => deleteTransaction(transaction.id) }/>
-)} 
-
+    <div className="space-y-4 max-h-[60vh] overflow-y-auto pr-1">
+      {transactions.map((transaction) =>
+        <TransactionItem
+          key={transaction.id}
+          title={transaction.title}
+          amount={transaction.amount}
+          onDelete={() => deleteTransaction(transaction.id)}
+        />
+      )}
     </div>
-  ) 
+  )
 }
 
-export default TransactionList 
+export default TransactionList
