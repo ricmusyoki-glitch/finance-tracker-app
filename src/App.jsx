833efd81e4c9a3b8518259d576ed094469c389d5
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-16 20:51:22 +0300

    feat: simplify route guards and handle authLoading at top level in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { Routes, Route } from "react-router-dom"
-
+import { Routes, Route, Navigate } from "react-router-dom"
+import useFinance from "./context/useFinance"
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
 import Transactions from "./pages/Transactions"
 import Profile from "./pages/Profile"
+import Loader from "./components/Loader"
 
 function App() {
+  const { user, authLoading } = useFinance()
+
+  if (authLoading) return <Loader />
+
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/transactions" element={<Transactions />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={user ? <Navigate to="/dashboard" replace /> : <Login />} />
+      <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/" replace />} />
+      <Route path="/transactions" element={user ? <Transactions /> : <Navigate to="/" replace />} />
+      <Route path="/profile" element={user ? <Profile /> : <Navigate to="/" replace />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
 
-export default App 
+export default App
