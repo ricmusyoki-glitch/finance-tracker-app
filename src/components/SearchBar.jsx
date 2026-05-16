94c73bd4979a27cd6d73fe4a5ac1348c666e2f32
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-16 20:53:38 +0300

    Improved the SearchBar with focus styling and a better placeholder text

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,8 @@ const SearchBar = ({search, setSearch}) => {
 
   return (
     <input type="text" 
-           placeholder="Search transactions"
-           className="border w-full p-3 rounded-lg mb-5"
+           placeholder="Search transactions..."
+           className="border w-full p-3 rounded-lg mb-5 outline-none focus:border-green-500"
            value={search} 
            onChange = {(e) => setSearch(e.target.value)}/>
   )       
