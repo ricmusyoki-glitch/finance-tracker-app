1c0850be19ae70a6f62f021efe8254fa4a00516b
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-16 12:02:54 +0300

    Modified the dashboard layout where the sidebar and the navbar are displayed

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,28 +1,22 @@
+import { useState } from "react"
 import Sidebar from "../components/Sidebar"
-import Navbar from "../components/Navbar" 
+import Navbar from "../components/Navbar"
 
-const Dashboard = ({ children }) => {
-
+const DashboardLayout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
-    <div className="flex">
-
-        <Sidebar />
-
-        <div className="flex-1">
-
-            <Navbar /> 
-
-            <main className="p-6">
-                  {children}
-
-            </main>
-
-        </div>
-
+      <div className="flex-1 flex flex-col overflow-auto">
+        <Navbar onMenuClick={() => setSidebarOpen(true)} />
+        <main className="p-4 md:p-6 flex-1">
+          {children}
+        </main>
+      </div>
     </div>
   )
 }
 
-export default Dashboard 
+export default DashboardLayout
