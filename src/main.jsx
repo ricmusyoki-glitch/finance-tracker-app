b87bbc49e91db94f79d6378cbfaee0e1f3414cc3
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-16 20:51:28 +0300

    Wrapped the app with FinanceProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import FinanceProvider from "./context/FinanceProvider"
+import "./index.css"
+import App from "./App.jsx"
 
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <FinanceProvider>
+        <App />
+      </FinanceProvider>
     </BrowserRouter>
-  </StrictMode>,
-) 
+  </StrictMode>
+)
