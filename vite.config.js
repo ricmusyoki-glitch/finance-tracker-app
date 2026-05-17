0d8e684a288e325fc44c85aa39ee02fe181b1c2f
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-17 21:27:12 +0300

    Fixed vite deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/finance-tracker-app/',
+export default defineConfig({ 
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
